77a2d13a7844bcde0f3f6847468a5c06ee378283
Matthew Briones <tallmatt5198@gmail.com>
2022-06-03 16:37:12 -0700

    Day 9 changes

--- 09_Day_Higher_order_functions/09_day_starter/scripts/main.js
+++ 09_Day_Higher_order_functions/09_day_starter/scripts/main.js
@@ -1,2 +1,17 @@
 console.log(countries)
 alert('Open the console and check if the countries has been loaded')
+
+const newCountries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const products = [
+  { product: 'banana', price: 3 },
+  { product: 'mango', price: 6 },
+  { product: 'potato', price: ' ' },
+  { product: 'avocado', price: 8 },
+  { product: 'coffee', price: 10 },
+  { product: 'tea', price: '' },
+]
+
+const countriesWithLand = newCountries.filter((country) => country.includes('land'));
+console.log(countriesWithLand);
