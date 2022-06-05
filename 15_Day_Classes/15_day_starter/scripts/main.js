593ed4de3b14af77a4f8a5a16e01bb78040f8314
Matt Briones <mbriones@uoregon.edu>
2022-06-05 12:44:32 -0700

    Day 15

--- 15_Day_Classes/15_day_starter/scripts/main.js
+++ 15_Day_Classes/15_day_starter/scripts/main.js
@@ -103,3 +103,87 @@ console.log(s1.getPersonInfo())
 console.log(s2.saySomething())
 console.log(s2.getFullName())
 console.log(s2.getPersonInfo())
+
+class Animal {
+  constructor(name, age, color, legs) {
+    this.name = name;
+    this.age = age;
+    this.color = color;
+    this.legs = legs;
+  }
+
+}
+
+class Dog extends Animal {
+  constructor(name, age, color, legs, breed) {
+    super(name, age, color, legs);
+    this.breed = breed;
+  }
+}
+
+ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
+
+class Statistics {
+  constructor(nums) {
+    this.nums = nums;
+  }
+
+  count() {
+    return this.nums.length;
+  }
+
+  sum() {
+    if(this.count() == 0) {
+      throw console.error("Empty array");
+    }
+    else {
+      return this.nums.reduce((acc, curr) => acc + curr, 0);
+    }
+  }
+
+  min() {
+    let minNum = 20000000;
+    for(const num of this.nums) {
+      if(num < minNum) {
+        minNum = num;
+      }
+    }
+    return minNum;
+  }
+
+  max() {
+    let maxNum = -200000;
+    for (const num of this.nums) {
+      if(num > maxNum) {
+        maxNum = num;
+      }
+    }
+    return maxNum;
+  }
+
+  range() {
+    return this.max() - this.min();
+  }
+
+  mean() {
+    return this.sum() / this.count();
+  }
+
+}
+
+const statistics = new Statistics(ages);
+
+console.log('Count:', statistics.count()) // 25
+console.log('Sum: ', statistics.sum()) // 744
+console.log('Min: ', statistics.min()) // 24
+console.log('Max: ', statistics.max()) // 38
+console.log('Range: ', statistics.range()) // 14
+console.log('Mean: ', statistics.mean()) // 30
+/*
+console.log('Median: ',statistics.median()) // 29
+console.log('Mode: ', statistics.mode()) // {'mode': 26, 'count': 5}
+console.log('Variance: ',statistics.var()) // 17.5
+console.log('Standard Deviation: ', statistics.std()) // 4.2
+console.log('Variance: ',statistics.var()) // 17.5
+console.log('Frequency Distribution: ',statistics.freqDist()) // [(20.0, 26), (16.0, 27), (12.0, 32), (8.0, 37), (8.0, 34), (8.0, 33), (8.0, 31), (8.0, 24), (4.0, 38), (4.0, 29), (4.0, 25)]
+*/
